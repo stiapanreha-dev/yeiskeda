a4f1ec87db415109f96a4a540b5b75aaa3f69164
Stiapan Reha <stiapan.reha@gmail.com>
2025-11-18 16:44:12 +0300

    Revert to single API key - fix geocoding error

--- frontend/src/pages/StoreSettings.jsx
+++ frontend/src/pages/StoreSettings.jsx
@@ -5,7 +5,6 @@ import { storesAPI } from '../services/api';
 import Layout from '../components/Layout';
 
 const YANDEX_API_KEY = import.meta.env.VITE_YANDEX_API_KEY;
-const YANDEX_GEOSUGGEST_KEY = import.meta.env.VITE_YANDEX_GEOSUGGEST_KEY || YANDEX_API_KEY;
 
 const StoreSettings = () => {
   const navigate = useNavigate();
@@ -95,7 +94,7 @@ const StoreSettings = () => {
 
     console.log('Loading Yandex Maps API script');
     const script = document.createElement('script');
-    script.src = `https://api-maps.yandex.ru/2.1/?apikey=${YANDEX_GEOSUGGEST_KEY}&lang=ru_RU`;
+    script.src = `https://api-maps.yandex.ru/2.1/?apikey=${YANDEX_API_KEY}&lang=ru_RU`;
     script.async = true;
     script.onload = () => {
       console.log('Yandex Maps script loaded');
